bbc2474879f16cfb8a2adcf71fd67d696e143368
YudGir <yudhagirsang07@gmail.com>
2026-04-05 18:54:20 +0700

    mengubah isi list dengan menambahkan list baru yang berisi angka genap saja

--- python_chalans_1.py
+++ python_chalans_1.py
@@ -1,56 +1,65 @@
-banyak_input_user = int(input("Berapa angka Anda ingin masukkan? "))
+# banyak_input_user = int(input("Berapa angka Anda ingin masukkan? "))
 
-angka_user = []
-jumlah_angka_genap = 0
-jumlah_angka_ganjil = 0
-total_angka_genap = 0
-total_angka_ganjil = 0
-i = 0
+# angka_user = []
+# jumlah_angka_genap = 0
+# jumlah_angka_ganjil = 0
+# total_angka_genap = 0
+# total_angka_ganjil = 0
+# i = 0
 
-for i in range (banyak_input_user):
-    angka = int(input("Masukkan angka: "))
-    angka_user.append(angka)
+# for i in range (banyak_input_user):
+#     angka = int(input("Masukkan angka: "))
+#     angka_user.append(angka)
 
-for angka in angka_user:
-    if angka % 2 == 0:
-        total_angka_genap += 1
-        jumlah_angka_genap += angka
-    else:
-        total_angka_ganjil += 1
-        jumlah_angka_ganjil += angka
+# for angka in angka_user:
+#     if angka % 2 == 0:
+#         total_angka_genap += 1
+#         jumlah_angka_genap += angka
+#     else:
+#         total_angka_ganjil += 1
+#         jumlah_angka_ganjil += angka
 
-print("Jumlah angka genap Anda adalah:", jumlah_angka_genap)
-print("Banyak angka genap Anda adalah:", total_angka_genap)
-print("Jumlah angka ganjil Anda adalah:",jumlah_angka_ganjil)
-print("Banyak angka ganjil Anda adalah:", total_angka_ganjil)
+# print("Jumlah angka genap Anda adalah:", jumlah_angka_genap)
+# print("Banyak angka genap Anda adalah:", total_angka_genap)
+# print("Jumlah angka ganjil Anda adalah:",jumlah_angka_ganjil)
+# print("Banyak angka ganjil Anda adalah:", total_angka_ganjil)
 
 
-angka_user = []
-angka = 1
-while True:
-    if angka != -1:
-        angka = int(input("Masukkan angka: "))
-        angka_user.append(angka)
-        i += 1
-    else:
-        angka_user.remove(angka) #ini ada biar angka -1 yg terakhir tidak dimasukkan ke list (atau di c++: array) angka_user
-        break
+# angka_user = []
+# angka = 1
+# while True:
+#     if angka != -1:
+#         angka = int(input("Masukkan angka: "))
+#         angka_user.append(angka)
+#         i += 1
+#     else:
+#         angka_user.remove(angka) #ini ada biar angka -1 yg terakhir tidak dimasukkan ke list (atau di c++: array) angka_user
+#         break
 
-banyak_muncul = 0
-modus = 0
+# banyak_muncul = 0
+# modus = 0
 
-for angka in angka_user:
-    jumlah = angka_user.count(angka)
+# for angka in angka_user:
+#     jumlah = angka_user.count(angka)
     
-    if jumlah > banyak_muncul:
-        banyak_muncul = jumlah
-        modus = angka
-print(f"Angka yang paling banyak muncul (modus): {modus}")
+#     if jumlah > banyak_muncul:
+#         banyak_muncul = jumlah
+#         modus = angka
+# print(f"Angka yang paling banyak muncul (modus): {modus}")
 
-print(f"Banyak angka: {angka_user.count(angka)}")
-print(f"Banyak angka Anda adalah: {len(angka_user)}")
-print(f"Angka terbesar (max)-nya adalah: {max(angka_user)}")
-print(f"Angka terkecil (min)-nya adalah: {min(angka_user)}")
-print(f"Angka rata-rata (average)-nya adalah: {int(sum(angka_user)/len(angka_user))}")
-# aku buat nilai averagenya jadi int, bukan float biar enak
+# print(f"Banyak angka: {angka_user.count(angka)}")
+# print(f"Banyak angka Anda adalah: {len(angka_user)}")
+# print(f"Angka terbesar (max)-nya adalah: {max(angka_user)}")
+# print(f"Angka terkecil (min)-nya adalah: {min(angka_user)}")
+# print(f"Angka rata-rata (average)-nya adalah: {int(sum(angka_user)/len(angka_user))}")
+# # aku buat nilai averagenya jadi int, bukan float biar enak
 
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+data_baru = [] #ini list baru guna nyimpen angka genap nantinya
+
+for x in data:
+    if x % 2 == 0:
+        angka_baru = x * 2 #kita simpan hasil dari angka genap tersebut yg dikalikan dengan 2 ke dalam variabel angka_baru
+        data_baru.append(angka_baru) #kita tambahkan setiap hasil perkalian ke dalam list baru, yaitu data_baru
+
+print(data_baru) #print, deh, hihi
