77cd205aaabfe3e5b9fd1cd93bca11696f66ba06
YudGir <yudhagirsang07@gmail.com>
2026-04-06 20:47:34 +0700

    Completed Simple Library Manager with Binary Search and Try-Except

---
+++ python_chalans_2.py
@@ -0,0 +1,69 @@
+#Simple Library Manager
+
+jumlahBuku = 0
+listjudulBuku = []
+
+def tambah_buku(banyakBuku):
+    for i in range (banyakBuku):
+        judulBuku = input("Input its title book here: ").lower()
+        listjudulBuku.append(judulBuku)
+    return True 
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return -1
+
+while True:
+    print("Hello, there.\n")
+    print("We got your list: \n")
+    print("1. Input your data (and program will automatically sort 'em all)\n")
+    print("2. Search a title\n")
+    print("3. Delete 'em from your list\n")
+    print("4. Print your sorted list, huh?\n")
+    print("5. Exit the program")
+
+    try:
+        userChoice = int(input("Wanna go? Type here the number you pick: "))
+
+        if userChoice == 1:
+            banyakBukuUser = int(input("How many books do you want to add to your own list? "))
+            variabel_penampung = tambah_buku(banyakBukuUser)
+            listjudulBuku.sort()
+        elif userChoice == 2:
+            searchingABook = input("What is the title of the book you're searching for? ").lower()
+            isFound = binary_search(listjudulBuku, searchingABook)
+
+            if isFound != -1:
+                print("In the list you created, it's found!\n")
+            else:
+                print("Sorry, we do afford yet we can't find it for you for now.\n")
+        elif userChoice == 3:
+            deletingABook = input("What is the title of the book you'd like to delete from? ").lower()
+            if deletingABook in listjudulBuku:
+                listjudulBuku.remove(deletingABook)
+                print("The book is already deleted from the list!\n")
+            else:
+                print("Can't find the book you'd like to delete from the list, sorry.\n")
+        elif userChoice == 4:
+            print(f"This is your sorted list: {listjudulBuku}")
+            print("remember, all of the title are lowercase 'cause we avoid crash program where user is a bit naughty, haha\n")
+        elif userChoice == 5:
+            print("Thank you for using this program, see ya in the future!\n")
+            break
+        else:
+            print("Hey, error input detected. Stay aware, please in inputting the number menu you pick.\n")
+            continue
+    
+    except Exception as e:
+        print(f"\nERROR! There's a problem here: {e}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,51 +1,63 @@
-# banyak_input_user = int(input("Berapa angka Anda ingin masukkan? "))
+#Simple Library Manager
 
-# angka_user = []
-# jumlah_angka_genap = 0
-# jumlah_angka_ganjil = 0
-# total_angka_genap = 0
-# total_angka_ganjil = 0
+jumlahBuku = 0
+listjudulBuku = []
 
-# for i in range (banyak_input_user):
-#     angka = int(input("Masukkan angka: "))
-#     angka_user.append(angka)
+def tambah_buku(banyakBuku):
+    for i in range (banyakBuku):
+        judulBuku = input("Input judul buku: ").lower()
+        listjudulBuku.append(judulBuku)
+    return True 
 
-# for angka in angka_user:
-#     if angka % 2 == 0:
-#         total_angka_genap += 1
-#         jumlah_angka_genap += angka
-#     else:
-#         total_angka_ganjil += 1
-#         jumlah_angka_ganjil += angka
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
 
-# print("Jumlah angka genap Anda adalah:", jumlah_angka_genap)
-# print("Banyak angka genap Anda adalah:", total_angka_genap)
-# print("Jumlah angka ganjil Anda adalah:",jumlah_angka_ganjil)
-# print("Banyak angka ganjil Anda adalah:", total_angka_ganjil)
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return -1
 
-i = 0
-angka_user = []
-angka = 1
 while True:
-    if angka != -1:
-        angka = int(input("Masukkan angka: "))
-        angka_user.append(angka)
-        i += 1
-    else:
-        angka_user.remove(angka) #ini ada biar angka -1 yg terakhir tidak dimasukkan ke list (atau di c++: array) angka_user
+    print("Hello, there.\n")
+    print("We got your list: \n")
+    print("1. Input your data (and program will automatically sort 'em all)\n")
+    print("2. Search a title\n")
+    print("3. Delete 'em from your list\n")
+    print("4. Print your sorted list, huh?\n")
+    print("5. Exit the program")
+    userChoice = int(input("Wanna go? Type here the number you pick: "))
+
+    if userChoice == 1:
+        banyakBukuUser = int(input("Berapa banyak buku Anda ingin tambahkan? "))
+        variabel_penampung = tambah_buku(banyakBukuUser)
+        listjudulBuku.sort()
+    elif userChoice == 2:
+        searchingABook = input("What is the title of the book you're searching for? ").lower()
+        isFound = binary_search(listjudulBuku, searchingABook)
+
+        if isFound != -1:
+            print("In the list you created, it's found!\n")
+        else:
+            print("Sorry, we do afford yet we can't find it for you for now.\n")
+    elif userChoice == 3:
+        deletingABook = input("What is the title of the book you'd like to delete from? ").lower()
+        if deletingABook in listjudulBuku:
+            listjudulBuku.remove(deletingABook)
+            print("The book is already deleted from the list!\n")
+        else:
+            print("Can't find the book you'd like to delete from the list, sorry.\n")
+    elif userChoice == 4:
+        print(f"This is your sorted list: {listjudulBuku}")
+    elif userChoice == 5:
+        print("Thank you for using this program, see ya in the future!\n")
         break
-
-for angka in angka_user:
-    # total_banyak_angkauser = angka_user.count(angka)
-    print(angka_user.count(angka))
-
-    if (angka_user.count(angka) > angka_user.count(angka + 1)):
-        angka_user_terbanyak_muncul = angka
-
-# print(f"Banyak angka: {angka_user.count(angka)}")
-# print(f"Banyak angka Anda adalah: {len(angka_user)}")
-# print(f"Angka terbesar (max)-nya adalah: {max(angka_user)}")
-# print(f"Angka terkecil (min)-nya adalah: {min(angka_user)}")
-# print(f"Angka rata-rata (average)-nya adalah: {int(sum(angka_user)/len(angka_user))}")
-# # aku buat nilai averagenya jadi int, bukan float biar enak
-print(f"Angka yang paling banyak muncul (modus): {angka_user_terbanyak_muncul}")
+    else:
+        print("Hey, error input detected. Stay aware, please in inputting the number menu you pick.\n")
+        continue
